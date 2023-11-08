e13a61ae7245ac3a637d7d3b6552935c392de104
juanfuc <juanfuc@gmail.com>
2023-11-07 22:28:04 -0500

    push de cambios en JuanFelipe

--- src/routes/juanf/+page.svelte
+++ src/routes/juanf/+page.svelte
@@ -1,2 +1,80 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { csv } from 'd3-fetch';
+    import { onMount } from 'svelte';
+
+    let cvsUrl;
+    let data = [];
+
+    async function consultarAPI(url) {
+        try {
+            const response = await csv(url);
+            data = response;
+            console.log(data);
+        } catch (error) {
+            console.error('Error al cargar datos CSV:', error);
+        }
+    }
+
+    onMount(() => {
+        if (cvsUrl) {
+            consultarAPI(cvsUrl);
+        }
+    });
+</script>
+
+<div class="contenedor-general">
+    <h1>Anotador</h1>
+
+    <form class="formulario-csv-url" on:submit|preventDefault={async () => await consultarAPI(cvsUrl)}>
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label>URL de una tabla CSV</label>
+        <input type="url" placeholder="URL" bind:value={cvsUrl} />
+        <button>Añadir datos</button>
+    </form>
+    <h3>URL de prueba:</h3> <a href="https://docs.google.com/spreadsheets/d/e/2PACX-1vS0a_VEphpTFGHjW4BF3_uihhL372X34cNfPd733zNkr4hbD29wOhVZyi42ta9biXoP94WoS_gW1Xwo/pub?output=csv">https://docs.google.com/spreadsheets/d/e/2PACX-1vS0a_VEphpTFGHjW4BF3_uihhL372X34cNfPd733zNkr4hbD29wOhVZyi42ta9biXoP94WoS_gW1Xwo/pub?output=csv</a>
+
+    {#if data.length > 0}
+        <h2>Datos cargados:</h2>
+        <div class="contenedor-tarjetas">
+            {#each data as d}
+            <div class="tarjeta"> 
+                <img src={d.imagen} alt="imagen">
+                <h1>{d.titulo}</h1> 
+                <h3>{d.autor}</h3>
+                <p>{d.año}</p>
+                <a href={d.manifiesto} target="_blank">IIIF Manifest</a>
+            </div>
+            {/each}
+        </div>
+    {:else if data.length === 0 && cvsUrl}
+        <p>Cargando datos...</p>
+    {:else}
+        <p>Esperando datos...</p>
+    {/if}
+</div>
+
+<style>
+    .contenedor-general {
+        margin: 5%;
+    }
+
+    .formulario-csv-url {
+        display: flex;
+        flex-direction: column;
+        width: 10%;
+    }
+    .contenedor-tarjetas {
+        display: flex;
+        flex-direction: row;
+        
+    }
+    .tarjeta {
+        width: 33%;
+        border: 1px solid black;
+        padding: 0.5%;
+        margin: 1%;
+    }
+    .tarjeta img {
+        width: 90vh;
+    }
+</style>
